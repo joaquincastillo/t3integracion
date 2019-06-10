d39a7455140673c9731e5740a068f86b1f0bb0ae
Joaquin Castillo <jacastillo@uc.cl>
2019-06-09 23:31:07 -0400

    first try graphQl queries v.0.4

--- t3/graphQL/queries.py
+++ t3/graphQL/queries.py
@@ -32,7 +32,8 @@ def get_film(filmId):
     client = GraphQLClient('https://swapi-graphql-integracion-t3.herokuapp.com/')
 
     query = '''
-        film(id: %s) {
+    {
+        film(id: %s ) {
             id
             episodeID
             title
@@ -40,28 +41,30 @@ def get_film(filmId):
             director
             producers
             releaseDate
-            starshipConnection {edges {node {...starshipFragment}}}
-            characterConnection {edges {node {...characterFragment}}}
-            planetConnection {edges {node {...planetFragment}}}
+            starshipConnection { edges { node { ...starshipFragment }}}
+            characterConnection { edges { node { ...characterFragment }}}
+            planetConnection { edges { node { ...planetFragment }}}
         }
+    }
+            
+    fragment starshipFragment on Starship {
+      id
+      name
+      model 
+      costInCredits
+    }
+    fragment characterFragment on Person {
+      name
+      id
+      species { name }
+    }
+    
+    fragment planetFragment on Planet {
+      name
+      id
+    }''' % filmId
 
-        fragment starshipFragment on Starship {
-            id
-            name
-            model
-            costInCredits
-        }
-        fragment characterFragment on Person {
-            name
-            id
-            species {name}
-        }
-
-        fragment planetFragment on Planet {
-            name
-            id
-        }
-        ''' % filmId
+    print(query)
 
     result = client.execute(query)
 
